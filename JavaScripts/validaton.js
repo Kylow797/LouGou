009ac0a7b14c99b7d1b1125b2828d2fc7164958f
Itzsadlife <108647602+Itzsadlife@users.noreply.github.com>
2022-07-06 00:53:15 +0800

    Update validaton.js

--- JavaScripts/validaton.js
+++ JavaScripts/validaton.js
@@ -1,21 +1,42 @@
-function FormValidation(){
-    //allert()
+<script>
+      function FormValidation() {
+      
+      var fullname = document.getElementById("fullname").value;
+      var email = document.getElementById("email").value;
+      var description = document.getElementById("description").value;
+      var orderNumber = /^[0-9A-Za-z]{6}-[0-9A-Za-z]{6}$/.test(document.getElementById("orderNumber").value)
+      var productNumber = /^[0-9A-Za-z]{5}-[0-9A-Za-z]{1}$/.test(document.getElementById("product").value)
 
-    var fullname = document.js_form.fullname;
-    var email = document.js_form.email;
+      if (fullname && email && description){
+          document.getElementById("fullname").style.border = "";
+          document.getElementById("email").style.border = "";
+          document.getElementById("description").style.border = "";
+          
+          if(orderNumber && productNumber) {
+              alert("Thank You For Contacting Us ! ");
+              document.getElementById("orderNumber").style.border = "";
+              document.getElementById("product").style.border = "";
+            }
 
+        } else if ((fullname && !email && !description) || 
+          (!fullname && !email && description) || 
+          (!fullname && email && !description) || 
+          (fullname && email && !description) || 
+          (!fullname && email && description) || 
+          (!fullname && !email && !description)){
+            alert("Please Enter All Information");
+            document.getElementById("fullname").style.border = "1px solid red";
+            document.getElementById("email").style.border = "1px solid red";
+            document.getElementById("description").style.border = "1px solid red";
 
- 
-
-     if (!email.value.match( /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) || email.value.match("")) {
-        email.nextElementSibling.style.display = "block";
-        email.style.border = "1px solid #f00";
-        return false
-     }else{
-        email.nextElementSibling.style.display = "none";
-        email.style.border = "1px solid transparent";
-     }
-
-
-     
+            if ((orderNumber && !productNumber) || 
+            (!orderNumber && !productNumber) || 
+            (!orderNumber && productNumber))
+            {
+              document.getElementById("orderNumber").style.border = "1px solid red";
+              document.getElementById("product").style.border = "1px solid red";
+              alert("Please Check Order Number and Product Number");
+            } 
+        }
     }
+</script>
